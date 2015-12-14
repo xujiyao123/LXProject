7ef428b2e5a97288ad0a0cbb3a5c47892601b890
刘旭 <472443138@qq.com>
2015-12-14 11:46:03 +0800

    new project

---
+++ Project_iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Project_iOS
+//
+//  Created by 刘旭 on 15/12/14.
+//  Copyright © 2015年 刘旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Project_iOS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Project_iOS
+//
+//  Created by 刘旭 on 15/12/14.
+//  Copyright © 2015年 刘旭. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
